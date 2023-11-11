2bb8de963fcc90bc42892fad7042e8d3fb31f55a
ArchanaY1203 <ay1840450@gmail.com>
2023-11-11 22:08:15 +0530

    Client Auth Routes Added

---
+++ client/src/utils/APIRoutes.tsx
@@ -0,0 +1,5 @@
+export const host = "http://localhost:8000";
+export const registerRoute = `${host}/auth/register`;
+export const loginRoute = `${host}/auth/login`;
+export const logoutRoute = `${host}/auth/logout`;
+export const userValidationRoute = `${host}/auth/userValidation`;
