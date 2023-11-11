cbff14a6c06974eab6b792d002f0bb64e79d5bf3
32bitdev <32bitdev@gmail.com>
2023-11-11 19:02:46 +0530

    Project Initiated

---
+++ readme.md
@@ -0,0 +1,51 @@
+# Doodles
+
+A Drawing and Guessing Game
+
+## Requirements
+
+MongoDB server should be running locally.
+
+## Deployment
+
+To deploy this project
+
+Open terminal and navigate to client
+
+```bash
+  cd motion/client
+```
+Install packages
+
+```bash
+  npm install
+```
+Now navigate to server
+
+```bash
+  cd ../server
+```
+Install packages
+
+```bash
+  npm install
+```
+
+Start the server
+
+```bash
+  npm start
+```
+
+Open a new terminal in client directory and start the app
+
+```bash
+  npm start
+```
+## Tech Stack
+
+React.ts, Node.js, MongoDB, Express.js, Socket.IO
+
+## Created by
+- [Subrat Pandey](https://github.com/32bitdev)
+- [Archana Yadav](https://github.com/archanay1203)
---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+
+app.use(express.json());
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Credentials', true);
+    res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.setHeader("Access-Control-Expose-Headers", "Content-Disposition");
+    res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Authorization, authorization, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+    next();
+});
+
+const server = app.listen(process.env.PORT, () =>
+    console.log(`Server started on port ${process.env.PORT}`)
+);
